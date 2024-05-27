76d8a190742fd85e100ec0c05a2c684e05367745
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2024-05-26 22:06:06 -0300

    fix cors free

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   dotenv.config();
   app.enableCors({ origin: true });
 
