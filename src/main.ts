6822085cc48f84938164a9cc4d49fffe82367ea7
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2024-05-27 07:54:47 -0300

    fix: adjustment cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,16 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   dotenv.config();
-  app.enableCors({ origin: true });
+  app.enableCors({
+    origin: [
+      'http://localhost',
+      'http://localhost:5173',
+      'http://seu-endereco-ec2.com.br',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: '*',
+});
 
   app.useGlobalPipes(new ValidationPipe());
   // de acordo com doc do prisma e do nest, agora é necessário para os escutadores de eventos de encerramento funcionarem
